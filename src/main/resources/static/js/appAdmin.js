b8e0cb562cf8c5f1877c4aef95e6a5653abfda57
NorbertKozma <norbiunivers@gmail.com>
2025-06-08 14:25:50 +0200

    modify logout3

--- src/main/resources/static/js/appAdmin.js
+++ src/main/resources/static/js/appAdmin.js
@@ -286,7 +286,7 @@ function logout() {
                 if (response.redirected) {
                     window.location.href = response.url;
                 } else {
-                    window.location.href = "/";
+                    window.location.href = "/showMyLoginPage";
                 }
 
                 // szám gombok aktiválása
